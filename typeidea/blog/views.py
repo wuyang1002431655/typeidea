49f593caa95d7c3b047c564341e587bde6a1f1a9
WuYang <1002431655@qq.com>
2020-02-15 14:54:31 +0800

    配置页面通用数据

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,24 +1,52 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
-from .models import Post, Tag
-
+from .models import Post, Tag, Category
+from config.models import SideBar
 
 def post_list(request, category_id=None, tag_id=None):
     # content = 'post_list category_id={category_id},tag_id={tag_id}'.format(category_id=category_id, tag_id=tag_id, )
     # return HttpResponse(content)
     # return render(request, 'blog/list.html', context={'name': 'post_list'})
+    tag = None
+    category = None
+
+    # if tag_id:
+    #     try:
+    #         tag = Tag.objects.get(id=tag_id)
+    #     except Tag.DoesNotExist:
+    #         post_lasts = []
+    #     else:
+    #         post_lasts = tag.post_set.filter(status=Post.STATUS_NORMAL)
+    # else:
+    #     post_lasts = Post.objects.filter(status=Post.STATUS_NORMAL)
+    #     if category_id:
+    #         try:
+    #             category = Category.objects.get(id=category_id)
+    #         except Category.DoesNotExist:
+    #             category = None
+    #     else:
+    #         post_lasts = post_lasts.filter(category_id=category_id)
+    #
+    # context={
+    #     'category':category,
+    #     'tag':tag,
+    #     'post_list':post_lasts
+    # }
+    # return render(request, 'blog/list.html', context=context)
     if tag_id:
-        try:
-            tag = Tag.objects.get(id=tag_id)
-        except Tag.DoesNotExist:
-            post_lasts = []
-        else:
-            post_lasts = tag.post_set.filter(status=Post.STATUS_NORMAL)
+        post_last, tag = Post.get_by_tag(tag_id)
+    elif category_id:
+        post_last, category = Post.get_by_category(category_id)
     else:
-        post_lasts = Post.objects.filter(status=Post.STATUS_NORMAL)
-        if category_id:
-            post_lasts = post_lasts.filter(category_id=category_id)
-    return render(request, 'blog/list.html', context={'post_list': post_lasts})
+        post_last = Post.latest_posts()
+    context = {
+        'category': category,
+        'tag': tag,
+        'post_list': post_last,
+        'sidebars': SideBar.get_all(),
+    }
+    context.update(Category.get_navs())
+    return render(request, 'blog/list.html', context=context)
 
 
 def post_detail(request, post_id):
@@ -28,7 +56,11 @@ def post_detail(request, post_id):
         post = Post.objects.get(id=post_id)
     except Post.DoesNotExist:
         post = None
-    return render(request, 'blog/detail.html', context={'post': post})
+    context={
+        'post':post,
+    }
+    context.update(Category.get_navs())
+    return render(request, 'blog/detail.html', context=context)
 
 
 '''
