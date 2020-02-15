0a8cc6f612c2ee5c909b671748d75739aec716b9
WuYang <1002431655@qq.com>
2020-02-15 17:35:11 +0800

    完成function view

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -19,13 +19,13 @@ from .custom_site import custom_site
 from blog.views import post_list, post_detail
 from config.views import links
 
-
 urlpatterns = [
-    url(r'^&', post_list),
-    url(r'^category/(?P<category_id>\d+)/$', post_list),
-    url(r'^tag/(?P<tag_id>)/$',post_list),
-    url(r'^post/(?P<post_id>\d+).html$',post_detail),
-    url(r'^links/$',links),
-    url(r'^admin/', custom_site.urls),
-    url(r'^super_admin/', admin.site.urls),
+    url(r'^&', post_list, name='index'),
+    url(r'^category/(?P<category_id>\d+)/$', post_list, name='category-list'),
+    url(r'^tag/(?P<tag_id>\d+)/$', post_list, name='tag-list'),
+    url(r'^post/(?P<post_id>\d+).html$', post_detail, name='post-detail'),
+    url(r'^links/$', links, name='links'),
+    url(r'^super_admin/', admin.site.urls, name='super-admin'),
+    url(r'^admin/', custom_site.urls, name='admin'),
 ]
+# url的reverse反向解析
